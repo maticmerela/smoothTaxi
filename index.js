51fabba2fa212758adcdae20c8a41d0219906ed3
FyFean <72227868+FyFean@users.noreply.github.com>
2022-05-17 00:20:51 +0200

    make it betteri

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
+app.use(express.static('browser'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
