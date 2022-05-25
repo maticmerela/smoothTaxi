754043abf5e5b330ee5e39c67e4e766e10ee8ba4
FyFean <72227868+FyFean@users.noreply.github.com>
2022-05-25 15:28:53 +0200

    yeppy

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const featurePolicy = require("feature-policy");
+app.use(
+  featurePolicy({
+    features: {
+      accelerometer: ["'*'"]
+    },
+  })
+);
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
-app.use(express.static('sensor-js.xyz'))
+
+
+
+app.use(express.static('browser'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
